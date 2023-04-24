065f8cff995ac514353becb7b21c7fbb071ac5a6
Xek0mb1k <oganes969@gmail.com>
2023-04-24 19:36:33 +0300

    Implementation weather view

--- app/src/main/java/com/xekombik/weatherapp/presentation/AboutActivity.kt
+++ app/src/main/java/com/xekombik/weatherapp/presentation/AboutActivity.kt
@@ -1,12 +1,22 @@
 package com.xekombik.weatherapp.presentation
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.xekombik.weatherapp.R
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val gitHubButton = findViewById<ImageButton>(R.id.githubLink)
+        gitHubButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Xek0mb1k/WeatherApp"))
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/xekombik/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/xekombik/weatherapp/presentation/MainActivity.kt
@@ -2,41 +2,129 @@ package com.xekombik.weatherapp.presentation
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.preference.PreferenceManager
 import com.xekombik.weatherapp.R
-import com.xekombik.weatherapp.databinding.ActivityChangeLocationBinding
 import com.xekombik.weatherapp.databinding.ActivityMainBinding
+import com.xekombik.weatherapp.domain.CurrentWeather
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private val vm by viewModel<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var sharedPreferences: SharedPreferences
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
         setSupportActionBar(binding.activityMainToolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        binding.locationTextView.setOnClickListener{
+
+        binding.locationTextView.setOnClickListener {
             val intent = Intent(this, ChangeLocationActivity::class.java)
             startActivity(intent)
         }
 
-        binding.currentDayCardView.setOnClickListener{
-            val intent = Intent(this, WeatherDetailActivity::class.java)
-            startActivity(intent)
+        CoroutineScope(Dispatchers.IO).launch {
+            val weather = vm.loadWeather("Moscow")
+
+            runOnUiThread {
+                refreshData(weather)
+            }
         }
+    }
+
+
+    @SuppressLint("SetTextI18n", "DiscouragedApi")
+    fun refreshData(weather: CurrentWeather) {
+
+        val stateLetter: Char
+        if (weather.current.is_day == 1) {
+            binding.mainCardLayout.background = ContextCompat.getDrawable(
+                this, R.drawable.day_linear_gradient
+            )
+            stateLetter = 'd'
+        } else {
+            binding.mainCardLayout.background = ContextCompat.getDrawable(this,
+                R.drawable.night_linear_gradient)
+            stateLetter = 'n'
+        }
+
+        binding.locationTextView.text = weather.location.name
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH)
+        val date = dateFormat.parse(weather.current.last_updated)
+
+        val formattedDateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.ENGLISH)
+        val formattedDate = date?.let { formattedDateFormat.format(it) }
+        binding.todayDataTextView.text = formattedDate
+
+        val parts = weather.current.condition.icon.split("/")
+        val imageName = stateLetter + parts.last().split(".")[0]
+
+        binding.weatherImageView.setImageResource(
+            resources.getIdentifier(imageName, "drawable", packageName)
+        )
+
+        val tempUnits = sharedPreferences.getString("temp_units", "º C")
+        if (tempUnits == "ºC") {
+            binding.weatherTodayTemperature.text = "${weather.current.temp_c}$tempUnits"
+        } else
+            binding.weatherTodayTemperature.text = "${weather.current.temp_f}$tempUnits"
+
+        binding.weatherTodayWeather.text = weather.current.condition.text
+
+
+        binding.lastUpdatedDataTextView.text =
+            "Last updated ${
+                weather.current.last_updated
+                    .split(" ")[1]
+                    .replace(":", ".")
+            }"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            R.id.about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
+
+
 //        val bu = findViewById<Button>(R.id.bu)
 //        val thb = findViewById<Button>(R.id.thb)
 //        val ndb = findViewById<Button>(R.id.ndb)
@@ -76,28 +164,3 @@ class MainActivity : AppCompatActivity() {
 //                }
 //            }
 //        }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_item, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-            R.id.about -> {
-                val intent = Intent(this, AboutActivity::class.java)
-                startActivity(intent)
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-}
